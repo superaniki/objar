989c520123bd583bda0005e10700c4f2f98614ca
Rickard Sandgren <rickardsandgren@gmail.com>
2012-10-19 16:53:56 +0200

    first commit - with devise login system

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Objar::Application.config.secret_token = '83d0addc668653ed3c09c211a6320a998c5609b2d47caee7f5787035df501719e03e5b6ca397ede3a31833b8bc60a02ed32eb15cb7aabbe1b3fea91645b17c4a'
